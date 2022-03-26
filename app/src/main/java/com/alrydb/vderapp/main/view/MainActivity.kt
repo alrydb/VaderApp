68cd1e543c32e2867a75e1cdb4c9ef40b7eee57f
alrydb <alrydb123@protonmail.com>
2022-03-26 11:21:01 +0100

    Meny

--- app/src/main/java/com/alrydb/vderapp/main/view/MainActivity.kt
+++ app/src/main/java/com/alrydb/vderapp/main/view/MainActivity.kt
@@ -6,17 +6,35 @@ import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
+import android.view.Menu
 import android.widget.Toast
 import com.alrydb.vderapp.R
+import com.alrydb.vderapp.databinding.ActivityMainBinding
 import com.alrydb.vderapp.main.viewmodel.WeatherInfoViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: WeatherInfoViewModel
+    private lateinit var binding : ActivityMainBinding
+
+
+    // Skapa menyn
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.options_menu, menu)
+
+        return true
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        //Rendera UI-element
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbarNav)
+        binding.toolbarNav.inflateMenu(R.menu.options_menu)
+
 
 
         //skapa viewmodel
@@ -24,16 +42,15 @@ class MainActivity : AppCompatActivity() {
 
 
 
-
+        // Kollar om platstjänsten är aktiverad
         if(!viewModel.isLocationEnabled(this)){
 
             Toast.makeText(this, "Platstjänsten är inte aktiverad", Toast.LENGTH_SHORT).show()
 
-            //Om platstjänsten inte är aktiverad så omdirigeras användaren till
+            //Om platstjänsten inte är aktiverad så omdirigeras användaren
             // Till mobilens inställningar för platstjänst
             val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
             startActivity(intent)
-
 
             }else{
 
